6f73f855ee79ef0a10f2c977689ec7fb6620e66f
ShanJifei <2572899924@qq.com>
2022-06-08 09:57:44 +0800

    6.8上午

---
+++ src/com/helloworld.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * gitee最后的妥协
+ *
+ * @author zxcsjf
+ * @since 2022/06/08 09:56
+ */
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+    }
+}
